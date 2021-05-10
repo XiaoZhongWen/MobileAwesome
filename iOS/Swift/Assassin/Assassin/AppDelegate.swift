a1a236b1921f5b60c1547d463d3d3f157a75a899
肖仲文 <262804333@qq.com>
2021-05-10 22:17:17 +0800

    Assassin swift

---
+++ iOS/Swift/Assassin/Assassin/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Assassin
+//
+//  Created by 肖仲文 on 2021/5/10.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
